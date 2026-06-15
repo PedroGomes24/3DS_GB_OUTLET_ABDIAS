6b621a05efc1a3ec34373f2388dfd241db08385e
PedroGomes24 <pedrinho2008gomes@gmail.com>
2026-06-15 10:58:32 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+function validarLogin(){
+    let login = document.getElementById('login').value; 
+    let senha = document.getElementById('senha').value; 
 
+    if (login === 'admin' && senha === 'senha') {
+        alert(`Oi, ${login}, sumido!`);
+        window.location.href = 'produtos.html'
+    } else {
+        alert(`Usuário ou senha inválidos, tente novamente.`);
+    }   
+
+    login = document.getElementById('login').value = ''; 
+    senha = document.getElementById('senha').value = ''; 
+}
